ffafd02e76b0a3e7625e15a58f93f84e159152f4
sooyeongit <sooyeonkimunam@ciencias.unam.mx>
2025-03-07 21:25:56 -0600

    Solucion al no reconocimiento del ACTIVITY_RECOGNITION

--- app/src/main/java/mx/unam/fciencias/moviles/duel/MainActivity.java
+++ app/src/main/java/mx/unam/fciencias/moviles/duel/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.Manifest;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
